3babaedf0cb98a76fe56a1b2ea41c7f932a2c7ef
Lekan Koku <kokulekan23@gmail.com>
2022-02-22 10:59:50 +0100

    feat: implemented state management + local storage

--- src/components/start-quiz.js
+++ src/components/start-quiz.js
@@ -1,10 +1,12 @@
+import store from '../store/index.js';
+
 var template = document.createElement('template')
 template.innerHTML= `
     <link rel="stylesheet" href="css/main.css" />
     <div class="start_page">
         <img alt = "logo" height="65" width ="65" src ="resources/logo.svg"/>
-        <input class="start_element" type="text" id="fname" name="fname" placeholder="Enter your name">
-        <button class="start_element">Start Quiz</button>
+        <input class="start_element name" type="text" id="fname" name="fname" placeholder="Enter your name">
+        <button class="start_element start">Start Quiz</button>
     </div>
     
 `
@@ -14,6 +16,18 @@ class StartQuiz extends HTMLElement {
         super();
         this.attachShadow({mode: 'open'});
         this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    initializeQuiz(){
+        const name = this.shadowRoot.querySelector(".name").value;
+        store.dispatch('startQuiz', { name });
+    }
+
+    connectedCallback(){
+        this.shadowRoot.querySelector(".start").addEventListener('click', () => this.initializeQuiz())
+    }
+    disconnectedCallback(){
+        this.shadowRoot.querySelector('.start').removeEventListener()
     }
 }
 
