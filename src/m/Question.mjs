4e95ba8d85b115c915984da31c613a42cbfb15fe
gw <g.wagner@b-tu.de>
2023-08-24 19:12:10 +0200

    update Question.mjs and design-generated-quiz.html

--- src/m/Question.mjs
+++ src/m/Question.mjs
@@ -2,15 +2,24 @@ import eNUMERATION from "../../lib/eNUMERATION.mjs";
 const QuestionTypeEL = new eNUMERATION("QuestionTypeEL", ["multichoice","shortanswer","numerical"]);
 
 class Question {
-    constructor({id, name, topic, type, questionText, questionTextItem, hasManyCorrectAnswers, answerOptions}) {
+    constructor({id, name, topic, type, questionText, questionTextItem,
+                  generalFeedback, generalFeedbackTextItem, hasManyCorrectAnswers,
+                  answerOptions, caseMustMatch,
+                  correctAnswers, partiallyCorrectAnswers, correctNumericalAnswer}) {
       this.id = id;
       this.name = name;
       this.topic = topic;
       this.type = type;
       this.questionText = questionText;
       if (questionTextItem) this.questionTextItem = questionTextItem;
+      this.generalFeedback = generalFeedback;
+      if (generalFeedbackTextItem) this.generalFeedbackTextItem = generalFeedbackTextItem;
       this.hasManyCorrectAnswers = hasManyCorrectAnswers;
-      this.answerOptions = answerOptions;
+      this.answerOptions = answerOptions;  // for multi-choice questions
+      if (caseMustMatch) this.caseMustMatch = caseMustMatch  // for short answer questions
+      if (correctAnswers) this.correctAnswers = correctAnswers;  // for short answer questions
+      if (partiallyCorrectAnswers) this.partiallyCorrectAnswers = partiallyCorrectAnswers;  // for short answer questions
+      if (correctNumericalAnswer) this.correctNumericalAnswer = correctNumericalAnswer;  // for numerical questions
     }
 }
 
